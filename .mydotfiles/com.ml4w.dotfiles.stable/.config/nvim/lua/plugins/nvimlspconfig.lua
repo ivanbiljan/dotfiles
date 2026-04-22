4be93e3d22e3135d37dd70eaa6de597be2d80719
ivanbiljan <ssbiljan@gmail.com>
2026-04-22 23:40:16 +0200

    Update nvim-lspconfig keybinds

--- .mydotfiles/com.ml4w.dotfiles.stable/.config/nvim/lua/plugins/nvimlspconfig.lua
+++ .mydotfiles/com.ml4w.dotfiles.stable/.config/nvim/lua/plugins/nvimlspconfig.lua
@@ -8,6 +8,20 @@ return {
                 require("mason-lspconfig").setup({
                     automatic_enable = true
                 })
+
+                vim.api.nvim_create_command("LspAttach", {
+                    callback = function(ev)
+                        local opts = { buffer = ev.buffer }
+                        vim.keymap.set("n", "gD", vim.lsp.buf.definition, opts)
+                        vim.keymap.set("n", "gd", vim.lsp.buf.declaration, opts)
+                        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+                        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+                        vim.keymap.set("n", "<leader>fc", function()
+                            vim.lsp.buf.format { async = true }
+                        end, opts)
+                    end
+                })
             end
         }
     }
