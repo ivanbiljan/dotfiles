4c0971245e22141cf3dbe586fa92d2b1a0828c0c
ivanbiljan <ssbiljan@gmail.com>
2026-04-25 01:43:30 +0200

    nvim: make <leader>pv automatically fallback to find_files

--- .mydotfiles/winhlp64/.config/nvim/after/plugin/telescope.lua
+++ .mydotfiles/winhlp64/.config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,9 @@
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>pf", function()
-    if not pcall(builtin.git_files) then
-        vim.notify("The current directory is not a git repository. Consider <C-p>", vim.log.levels.WARN)
+    local ok = pcall(builtin.git_files)
+    if not ok then
+        vim.notify("Not a git repository - falling back to find_files", vim.log.levels.WARN)
+        builtin.find_files()
     end
 end, {})
 vim.keymap.set("n", "<C-p>", builtin.find_files, {}) 
