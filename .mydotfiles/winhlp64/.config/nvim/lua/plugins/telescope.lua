849841bdc26951c7eaff6944cc008218f199f8a9
ivanbiljan <ssbiljan@gmail.com>
2026-04-25 02:04:31 +0200

    Add file_ignore_patterns to telescope config

--- .mydotfiles/winhlp64/.config/nvim/lua/plugins/telescope.lua
+++ .mydotfiles/winhlp64/.config/nvim/lua/plugins/telescope.lua
@@ -5,5 +5,20 @@ return
         'nvim-lua/plenary.nvim',
         -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
+    },
+    config = function()
+        local telescope = require("telescope")
+        telescope.setup {
+            defaults = {
+                file_ignore_patterns = {
+                    "%.git/",
+                    "bin/",
+                    "obj/",
+                    "node_modules/",
+                    "vendor/",
+                    "dist/",
+                }
+            }
+        }
+    end
 }
