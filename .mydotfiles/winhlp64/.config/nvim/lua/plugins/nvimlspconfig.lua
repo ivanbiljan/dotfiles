b1266f15a2576d36d7d11292bd95b2b533171e7c
ivanbiljan <ssbiljan@gmail.com>
2026-05-14 00:24:05 +0200

    Update nvimlspconfig and hypr config

--- .mydotfiles/winhlp64/.config/nvim/lua/plugins/nvimlspconfig.lua
+++ .mydotfiles/winhlp64/.config/nvim/lua/plugins/nvimlspconfig.lua
@@ -12,6 +12,7 @@ return {
                 vim.api.nvim_create_autocmd("LspAttach", {
                     callback = function(ev)
                         local opts = { buffer = ev.buffer }
+                        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
                         vim.keymap.set("n", "gD", vim.lsp.buf.definition, opts)
                         vim.keymap.set("n", "gd", vim.lsp.buf.declaration, opts)
                         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
